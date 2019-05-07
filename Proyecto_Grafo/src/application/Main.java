fc122a3d852f6ff5740c69ddfe5d35179af5dab0
Juan-Puerta <42070035+Juan-Puerta@users.noreply.github.com>
2019-05-07 11:52:33 -0500

    I created the serializable methods and the folder where the data will be save

--- Proyecto_Grafo/src/application/Main.java
+++ Proyecto_Grafo/src/application/Main.java
@@ -1,13 +1,41 @@
 package application;
 	
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+
 import javafx.application.Application;
 import javafx.stage.Stage;
+import world.University;
 import javafx.scene.Scene;
 import javafx.scene.layout.AnchorPane;
 import javafx.fxml.FXMLLoader;
 
 
 public class Main extends Application {
+	
+	private University theIcesiUniversity;
+	
+	public Main() {
+		// TODO Auto-generated constructor stub
+		theIcesiUniversity = new University("Universidad Icesi", 22);
+	}
+	
+	public void serializable() throws FileNotFoundException, IOException {
+		ObjectOutputStream serializeUniversity = new ObjectOutputStream(new FileOutputStream("Data/dataGraph"));
+		serializeUniversity.writeObject(theIcesiUniversity);
+		serializeUniversity.close();
+	}
+	
+	public void deserialize() throws Exception {
+		ObjectInputStream unserializeUniversity = new ObjectInputStream(new FileInputStream("Data/dataGraph"));
+		theIcesiUniversity = (University) unserializeUniversity.readObject();
+		unserializeUniversity.close();
+	}
+	
 	@Override
 	public void start(Stage primaryStage) {
 		try {
