db245c07cd60138ab13faadf982e95c9ba12ff6e
camiilocq <38769518+camiilocq@users.noreply.github.com>
2019-05-27 17:19:11 -0500

    this has been changed

--- Proyecto_Grafo/src/application/TourController.java
+++ Proyecto_Grafo/src/application/TourController.java
@@ -19,11 +19,32 @@ public class TourController {
 	@FXML private Button salir;
 	
 	public void initialize() {
-		
+		salida.getItems().add("Edificio A");
+		salida.getItems().add("Edificio B");
+		salida.getItems().add("Edificio C");
+		salida.getItems().add("Edificio D");
+		salida.getItems().add("Edificio E");
+		salida.getItems().add("Edificio F");
+		salida.getItems().add("Edificio G");
+		salida.getItems().add("Edificio I");
+		salida.getItems().add("Edificio J");
+		salida.getItems().add("Edificio K");
+		salida.getItems().add("Edificio L");
+		salida.getItems().add("Edificio M");
+		salida.getItems().add("Edificio N");
+		salida.getItems().add("Edificio SAMÁN");
+		salida.getItems().add("Edificio AUDITORIOS");
+		salida.getItems().add("Edificio CENTRAL");
+		salida.getItems().add("Edificio BIBLIOTECA");
+		salida.getItems().add("Edificio CABALLERIZA");
+		salida.getItems().add("Edificio CANCHA 7");
+		salida.getItems().add("Edificio CANCHA 11");
+		salida.getItems().add("Edificio COLISEO 1");
+		salida.getItems().add("Edificio COLISEO 2");
 	}
 	
 	public void calcular() {
-		
+		System.out.println(salida.getValue());
 	}
 	
 	public void salir(Event event) {
